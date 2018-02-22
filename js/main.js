639820bcce548193f90c6eb883637fbf44584e44
Matthias De Vriendt <matthias@matthiasdv.com>
2018-02-22 21:07:38 +0100

    display only grid

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@
 // so we'll have to keep a list of transformations.
 // to draw the perspective of a specific neuron, apply the transformation to each point of the input layer or original set
 
+// learning in the browser is absolute madness
+// js should only be the presentation / animation layer
+// run learning algorithm in octave
+// after each iteration, get the parameters for each node (or edge?)
+// re-read colah's article before writing any code to get transformation parameters
+// THINK, before you code
+const synaptic = window.synaptic;
+
 // drawing is functional because matrices only store numbers
 function renderVertex(ctx, x, y, offsetX, offsetY, value, radius) {
   ctx.beginPath();
@@ -30,35 +38,24 @@ async function initMatrices(set) {
   return {
     m: set.length,
     g: 400,
-    set: set,
-    net: makeNeuralNet()
+    set: prepSet(set),
+    trainer: new synaptic.Trainer(new synaptic.Architect.Perceptron(2, 3, 2))
   }
 }
 
-function makeNeuralNet() {
-  const layer_defs = [];
-
-  // input layer of size 1x1x2 (all volumes are 3D)
-  layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:2});
-
-  // hidden layer
-  layer_defs.push({type:'fc', num_neurons:2, activation:'tanh'});
-
-  // a softmax classifier predicting probabilities for two classes: 0,1
-  layer_defs.push({type:'softmax', num_classes:2});
-
-  // create a net out of it
-  const net = new convnetjs.Net();
-  net.makeLayers(layer_defs);
-
-  return net;
+function prepSet(set) {
+  return set.map(el => ({
+    input: [Math.round(el.one), Math.round(el.two)],
+    output: [1-el.result, 0+el.result] // 1-x 0+x  yields [1,0] for Y=0 and [0,1] for Y=1
+  }))
 }
 
+
 function loop(conf) {
-  const G = Matrix.zeros(conf.g, 2); // to display the grid
-  const X = Matrix.zeros(conf.m, 2); // m x 2 matrix
-  const Y = Matrix.zeros(conf.m, 1); // vector of length m
-  const gridDistribution = [];
+  console.log(conf.set);
+  const G = new Array(conf.g); // to display the grid
+  const X = new Array(conf.m); // m x 2 matrix
+  const Y = new Array(conf.m); // vector of length m
 
   // canvas
   const sketch = Sketch.create({
@@ -84,42 +81,12 @@ function loop(conf) {
     // init grid vertices
     for (let k=0 ; k<gr; k++) {
       for (let l=0 ; l<gr; l++) {
-        G.set(k*gr+l, 0, k*wg);
-        G.set(k*gr+l, 1, l*hg);
+        G[k*gr+l] = [k*wg, l*hg];
       }
-    }
-
-    // init data vertices
-    for (let i=0 ; i<conf.m; i++) {
-      X.set(i, 0, conf.set[i].one * wu);
-      X.set(i, 1, conf.set[i].two * hu);
-      Y.set(i, 0, conf.set[i].result)
     }
   };
 
   sketch.update = function() {
-    // train neural net on sample data
-    const timeStart = Date.now();
-    console.log('training...');
-
-    const trainer = new convnetjs.Trainer(conf.net, {method: 'adadelta', l2_decay: 0.001, batch_size: 10});
-
-    for (let i=0; i<conf.m; i++) {
-      // console.log(X.get(i, 0), X.get(i, 1), Y.get(i, 0));
-      const x = new convnetjs.Vol([X.get(i, 0), X.get(i, 1)]);
-      trainer.train(x, Y.get(i, 0)); // this could simply be an array
-      // NEXT TIME: Take a look at the trainer in the docs. Training should provide probabilities
-    }
-
-    const timeStop = Date.now();
-    // console.log(`done! training took ${timeStop - timeStart}ms`);
-
-    // forward the grid points to plot the decision boundry
-    for (let j=0; j<conf.g; j++) {
-      const x = new convnetjs.Vol([G.get(j, 0), G.get(j, 1)]);
-      gridDistribution[j] = conf.net.forward(x);
-      // console.log(G.get(j, 0), G.get(j, 1), gridDistribution[j].w);
-    }
   };
 
   sketch.draw = function() {
@@ -141,18 +108,9 @@ function loop(conf) {
 
     // render grid vertices
     for (let j=0; j<conf.g; j++) {
-      const xCoord = G.get(j, 0);
-      const yCoord = G.get(j, 1);
-      renderVertex(sketch, xCoord, yCoord, oX, oY, gridDistribution[j].w[1] > .5, .5);
-    }
-
-    // render data vertices
-    for (let i=0; i<conf.m; i++) {
-      const xCoord = X.get(i, 0);
-      const yCoord = X.get(i, 1);
-      const label = Y.get(i, 0);
-
-      renderVertex(sketch, xCoord, yCoord, oX, oY, label, 3);
+      const xCoord = G[j][0];
+      const yCoord = G[j][1];
+      renderVertex(sketch, xCoord, yCoord, oX, oY, undefined, .5);
     }
   };
 }
